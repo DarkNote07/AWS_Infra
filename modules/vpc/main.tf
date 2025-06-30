37ef59a93ca545269dd1ce4861385870d171144e
DarkNote07 <jacobsoulis@yahoo.gr>
2025-06-30 14:00:58 +0000

    Pivate routing to nat gw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -72,3 +72,28 @@ resource "aws_eip" "lb" {
   domain   = "vpc"
 }
 
+resource "aws_route_table_association" "public" {
+
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  count = 2
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example[count.index].id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+
+  count = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
